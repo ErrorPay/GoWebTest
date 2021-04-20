eca27172f65f553b3f029f8b4aaae359be1d262c
Zheng Linxuan <zhenglinxuan@hotmail.com>
2021-04-20 09:11:19 +0800

    Add things

--- api/main.go
+++ api/main.go
@@ -3,11 +3,11 @@
 package api
 
 import (
-	"PingLeMe-Backend/conf"
-	"PingLeMe-Backend/model"
-	"PingLeMe-Backend/serializer"
 	"encoding/json"
 	"fmt"
+	"go-web-server/conf"
+	"go-web-server/model"
+	"go-web-server/serializer"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
